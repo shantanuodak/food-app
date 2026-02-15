5c358aeaea85878242a5f9d13bcdf5051f2910b1
Shantanu Odak <shantanuodak@yahoo.com>
2026-02-15 15:49:45 -0500

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Shantanu Odak on 2/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Shantanu Odak on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
